684dfc71e62fb6fab340951705dd5e7028968d2b
hwangej <hij3360@naver.com>
2024-03-11 13:53:51 +0900

    Add homepage

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,16 @@
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
-import { useEffect } from "react";
-import { isCompositeComponent } from "react-dom/test-utils";
+//import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
+import { useEffect, useCallback } from "react";
+//import { isCompositeComponent } from "react-dom/test-utils";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
-  };
+  }, [id]);
   useEffect(() => {
     getMovie();
   }, []);
